7303cb464d1f8fd836bd815d3e9076e98a884a44
loukeeehh <louka.constant@gmail.com>
2024-05-06 16:14:59 +0200

    Mon BlackJack

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿namespace blackjack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Bienvenue au BlakJack !");
+            int[,] TScoreUsers; // Matrice des scores de joueurs
+            int numPioche = 1; // rajouter une carte
+            int totalPoints = 0;
+            string nUser; //réponse de l'utilisateur
+            string message;
+            string repeatProg; // répétition du programme
+            mesMorceaux morceauxProg = new mesMorceaux(); // morceaux de programme
+
+            do
+            {
+                Console.WriteLine("Avant de vouloir commencer cette partie, voulez-vous les règles ?");
+                Console.WriteLine(" - Taper 1 pour les voir.\n - Taper 2 pour les passer.");
+                nUser = Console.ReadLine();
+
+
+                if (nUser == "1")
+                {
+                    Console.WriteLine("Voici les règles :");
+                    Console.WriteLine("Le Blackjack consiste à battre la banque, qui est représenté par le croupier, sans dépasser la valeur de 21 sinon vous perdez la partie.\nSi vous atteignez le Blackjack (une carte valant 10 + un as) vous gagnez directement la partie ! Si vous gagnez contre le croupier (si la valeur de vos cartes est supérieur à celle du croupier et après en avoir tiré 1 ou 2), vous remportez également la partie .\r\nIl existe des valeurs sur toutes les cartes (sauf le joker car il n'est présent dans le jeu). \n Les voici :\n\n Le BlackJack = 21 (Vous gagnez automatiquement)\n L'As = 1 ou 11 (Selon votre score, à votre avantage)\n Le roi = 10\n La reine = 10\n Le valet = 10\n La carte dix = 10\n La carte neuf = 9\n La carte huit = 8\n La carte sept = 7\n La carte six = 6\n La carte cinq = 5\n La carte quatre = 4\n La carte trois = 3\n La carte deux = 2\n");
+                }
+
+                else if (nUser == "2")
+                {
+                    Console.WriteLine("Très bien, je vous souhaite une bonne partie !\n");
+                }
+
+                Console.WriteLine("Voulez - vous commencer la partie ? 'o' = oui, 'n' = non");
+                nUser = Console.ReadLine();
+
+                if (nUser == "o")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    morceauxProg.genereMatrice(out TScoreUsers);
+                    morceauxProg.concateneMatrice(TScoreUsers, out message);
+                    Console.WriteLine($"{message}\n");
+                    morceauxProg.afficherTableauJoueurs(TScoreUsers, out message);
+                    Console.WriteLine(message);
+
+                    
+
+                    while (totalPoints < 21)
+                    {
+                        numPioche = 1;
+
+                        for (int i = 0; i <= TScoreUsers.GetLength(0) - 1; i++)
+                        {
+
+                            Console.WriteLine("Voulez-vous tirer une carte ? 't' pour tirer une carte, 'r' pour rester ");
+                            nUser = Console.ReadLine();
+
+                            if (nUser == "t")
+                            {
+                                morceauxProg.tireCarte(ref TScoreUsers, numPioche, i);
+                                
+                            }
+                            
+                        }
+                        
+
+                        Console.WriteLine("Vos cartes :");
+                        morceauxProg.concateneMatrice(TScoreUsers, out message);
+                        Console.WriteLine($"{message}\n");
+                        Console.Write("Les résultats :\n");
+                        morceauxProg.totalScoreJoueurs(TScoreUsers, numPioche, out totalPoints);
+                        Console.WriteLine($"Votre score est de {totalPoints}");
+
+                        
+                    }
+                    
+
+                    if (totalPoints > 21)
+                    {
+                        Console.WriteLine("Vous avez perdu !");
+                    }
+
+                    if (totalPoints == 21)
+                    {
+                        Console.WriteLine("Bravo, vous avez gagné !");
+                    }
+
+                    numPioche += 1;
+
+
+                }
+
+                // Demande de répetition 
+                Console.WriteLine("Voulez-Vous recommencer une partie ? 'o' = oui, 'n' = non.");
+                repeatProg = Console.ReadLine();
+
+            } while (repeatProg == "o");
+        }
+    }
+}
+    
+
---
+++ mesMorceaux.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    public struct mesMorceaux
+    {
+        /*
+        public void genereTableauJoueurs(out int[] T)
+        {
+
+            T = new int[6];
+            Random alea = new Random();
+
+            for (int i = 0; i <= T.Length - 5; i++)
+            {
+
+                T[i] = alea.Next(1, 11);
+
+            }
+
+        }
+        */
+        public void sommeTableau(ref int[] T, out int sum)
+        {
+            sum = T.Sum();
+
+        }
+
+        public void tireCarte(ref int[,] T, int numPioche, int numJoueur /* lignes */)
+        {
+
+            int carte = new Random().Next(1, 12);
+            T[numJoueur, numPioche + 1] = carte;
+             
+        }
+
+        public void totalScoreJoueurs(int[,] T, int numJoueur /*lignes*/, out int total)
+        {
+            int i;
+            total = 0;
+            for (i = 0; i <= T.GetLength(1) - 1; i++)
+            {
+                total = total + T[numJoueur, i];
+            }
+            
+        }
+
+
+        public void genereMatrice(out int[,] Matrice)
+        {
+            int lignes = 5;
+            int colonnes = 6;
+            Matrice = new int[lignes, colonnes];
+            int i;
+            int j;
+            Random alea = new Random();
+
+            for (i = 0; i < lignes; i++)
+            {
+
+                for (j = 0; j < colonnes; j++)
+                {
+                    if (j == 0 || j == 1)
+                    {
+                        Matrice[i, j] = alea.Next(1, 11);
+                    }
+
+                    else
+                    {
+                        Matrice[i, j] = 0;
+                    }
+                    
+                }
+            }
+
+        }
+
+
+        public void concateneMatrice(int[,] Matrice, out string concatene)
+        {
+            concatene = "";
+
+            for (int i = 0; i <= Matrice.GetLength(0) - 1; i++)
+            {
+                string lignes = "";
+
+                for (int j = 0; j <= Matrice.GetLength(1) - 1; j++)
+                {
+                    lignes = lignes + Matrice[i, j];
+
+                    if (j != Matrice.GetLength(1) - 1)
+                    {
+                        lignes = lignes + ",";
+                    }
+                }
+                concatene = concatene + "\n" + lignes;
+            }
+        }
+
+
+        public void afficherTableauJoueurs(int[,] T, out string message)
+        {
+            message = " ";
+            foreach (int i in T)
+            {
+                switch (i)
+                {
+                    case 1:
+
+                        message += "As ";
+                        break;
+                    case 2:
+
+                        message += "Deux ";
+                        break;
+
+                    case 3:
+
+                        message += "Trois ";
+                        break;
+
+                    case 4:
+
+                        message += "Quatre ";
+                        break;
+
+                    case 5:
+
+                        message += "Cinq ";
+                        break;
+
+                    case 6:
+
+                        message += "Six ";
+                        break;
+
+                    case 7:
+
+                        message += "Sept ";
+                        break;
+
+                    case 8:
+
+                        message += "Huit ";
+                        break;
+
+                    case 9:
+
+                        message += "Neuf ";
+                        break;
+
+                    case 10:
+                        message += "Dix ";
+                        break;
+
+                    case 11:
+                        message += "As ";
+                        break;
+                    default:
+
+                        message += " ";
+                        break;
+
+
+                }
+
+            }
+        }
+
+        
+
+
+
+
+        /*
+        public void annonceVainqueur(int scoreCroupier, int scoreUser, out string message)
+        {
+            message = "";
+            if (scoreUser <= 21 && scoreUser > scoreCroupier || scoreUser == 21)
+            {
+                message = $"Félicitation, vous avez gagné ! Votre score est de {scoreUser}";
+            }
+
+            else if (scoreUser < scoreCroupier)
+            {
+                message = $"Désolé, vous avez perdu ! Votre score est de {scoreUser}";
+            }
+
+            else if (scoreUser == scoreCroupier)
+            {
+                message = "Vous êtes à égalité avec le croupier !";
+            }
+
+        }
+        */
+
+      
+
+
+
+
+        /*
+        public void CarteCachee( ref int[] T)
+        {
+            int i;
+
+            for (i = 0; i < T.Length ; i++)
+            {
+                Console.WriteLine("Cartes cachées");
+            }
+        }
+        */
+        /*
+        public void annonceVainqueur(int scoreCroupier, int scoreUser, out string message)
+        {
+            message = "";
+            if (scoreUser <= 21 && scoreUser > scoreCroupier || scoreUser == 21)
+            {
+                message = $"Félicitation, vous avez gagné ! Votre score est de {scoreUser}";
+            }
+
+            else if (scoreUser < scoreCroupier)
+            {
+                message = $"Désolé, vous avez perdu ! Votre score est de {scoreUser}";
+            }
+
+            else if (scoreUser == scoreCroupier)
+            {
+                message = "Vous êtes à égalité avec le croupier !";
+            }
+
+        }
+        */
+    }
+}
